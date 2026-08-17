931a3b715c82f7c22112fab7d6b4c2aa71343b4f
Emil Storgaard <est@novicell.dk>
2026-08-17 15:52:35 +0200

    Fix umbraco page alias

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,7 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch }) => {
 	const [errorPage] = await Promise.all([
-		getContentByPath('errorPage', fetch)
+		getContentByPath('fejlside', fetch)
 			.then((d) => d as ErrorPage)
 			.catch(() => null)
 	]);
